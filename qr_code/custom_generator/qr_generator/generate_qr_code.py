8701803867ffdb7414bbd9aafba8993ea5ab71f5
laurent <laurent@gamercraft.com>
2024-05-01 20:59:49 +0200

    Add quiet zone wrapper

--- qr_code/custom_generator/qr_generator/generate_qr_code.py
+++ qr_code/custom_generator/qr_generator/generate_qr_code.py
@@ -6,3 +6,13 @@ def generate_qr_code(url: str) -> np.array:
     Generate a QR code matrix corresponding to the given url.
     """
     return np.array
+
+
+def add_quiet_zone(matrix: np.array, quiet_zone_size: int) -> np.array:
+    """
+    Add the quiet zone around the matrix.
+    """
+    result = np.zeros([size + 2 * quiet_zone_size for size in matrix.shape])
+    result = np.array(result, dtype=bool)
+    result[quiet_zone_size:-quiet_zone_size,quiet_zone_size:-quiet_zone_size] = matrix
+    return result

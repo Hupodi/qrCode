5f5ac26036ed0edcc9d1977a3a94f16056303158
laurent <laurent@gamercraft.com>
2024-05-21 15:23:37 +0200

    Removed testing lines

--- qr_code/generate_using_custom.py
+++ qr_code/generate_using_custom.py
@@ -1,11 +1,10 @@
 from qr_code.custom_generator import generate_qr_code, plot_png
-from qr_code.custom_generator.qr_generator.error_correction_level import ErrorCorrectionLevel
 
 
-def generate(url: str, output_path: str = "test_output2.png") -> None:
+def generate(url: str, output_path: str = "test_output.png") -> None:
     """
     Generate the QR code corresponding to the given URL
     Stores it in a png file.
     """
-    matrix = generate_qr_code(url=url, error_correction_level=ErrorCorrectionLevel.L)
+    matrix = generate_qr_code(url=url)
     plot_png(matrix=matrix, output_file=output_path)
